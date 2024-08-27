e967a420dee0356a6c3bbd62d7e72a665ad2ccfb
abdelhaq mahmoudi <abdelhak572@gmail.com>
2024-08-27 23:17:18 +0200

    imojis problem fixed

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,10 +38,13 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '3306',
         'OPTIONS': {
-            'init_command': 'SET default_storage_engine=INNODB',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1, \
+                             NAMES 'utf8mb4' COLLATE 'utf8mb4_unicode_ci'",
+            'charset': 'utf8mb4',
         },
     }
 }
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
